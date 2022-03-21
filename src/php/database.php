eacac1817c1754bdc9593af721795bb6e02044c7
JH-TechInfo <jh.techinfo21@gmail.com>
2022-03-21 22:27:44 +0530

    db rm cont

--- src/php/database.php
+++ src/php/database.php
@@ -4,10 +4,10 @@ header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json; charset=UTF-8");
-$db_host = 'localhost'; //31.170.160.103
-$db_name = 'u950689955_b2w';
-$db_username = 'u950689955_sab2w';
-$db_password = 'Jesus@123';
+$db_host = 'localhost'; 
+$db_name = 'u950689955';
+$db_username = 'u950689955';
+$db_password = '';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
 if ($mysqli->connect_error) {
